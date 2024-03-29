54c8fdfbe182aaed5fa53b1bc3daed627616b365
Naved Uddin <naveduddin83@gmail.com>
2024-03-29 14:02:28 +0530

    expressjs app folder structured

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const app = express();
 const path = require('path');
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.sendFile('index');
 });
 
 app.listen(PORT, () => {
